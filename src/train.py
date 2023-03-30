efaa64dc80c801e6ad8c6aeef4931bcfc45c18a7
Leon Kajikawa <0131reon254@gmail.com>
2023-03-30 13:32:28 +0900

    base

--- src/train.py
+++ src/train.py
@@ -55,110 +55,108 @@ def compute_metrics(p: EvalPrediction):
     preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
     preds = np.argmax(preds, axis=1)
     f1 = f1_score(p.label_ids, preds)
+    qwk = cohen_kappa_score(p.label_ids,preds,weights='quadratic')
     return {
         "f1_score": f1,
+        'QWK': qwk,
     }
 
-
-def train(args, X_train, y_train, X_test):
+# 訓練と検証
+def train(args, X_train, y_train,X_dev,y_dev,X_train):
     y_preds = []
-    oof_train = np.zeros((len(X_train),))
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     X_train = [tokenizer(text, padding="max_length", max_length=args.max_length, truncation=True) for text in X_train]
     X_test = [tokenizer(text, padding="max_length", max_length=args.max_length, truncation=True) for text in X_test]
-    test_dataset = HateSpeechDataset(X_test)
+    test_dataset = GradDataset(X_test)
 
-    skf = StratifiedKFold(n_splits=args.kfold, shuffle=True, random_state=args.seed)
-    for kfold_idx, (tra_idx, val_idx) in enumerate(skf.split(X_train, y_train)):
-        X_tra = [X_train[i] for i in tra_idx]
-        X_val = [X_train[i] for i in val_idx]
-        y_tra = [y_train[i] for i in tra_idx]
-        y_val = [y_train[i] for i in val_idx]
+    training_dataset = GradDataset(X_train, y_train)
+    validation_dataset = GradDataset(X_dev, y_dev)
 
-        training_dataset = HateSpeechDataset(X_tra, y_tra)
-        validation_dataset = HateSpeechDataset(X_val, y_val)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name)
 
-        model = AutoModelForSequenceClassification.from_pretrained(args.model_name)
+    training_args = TrainingArguments(
+        output_dir=os.path.join(args.save_model_dir, f"fold{kfold_idx}"),
+        overwrite_output_dir=True,
+        evaluation_strategy="steps",
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        learning_rate=args.learning_rate,
+        num_train_epochs=args.epochs,
+        log_level="critical",
+        logging_steps=25,
+        save_strategy="steps",
+        save_steps=25,
+        save_total_limit=3,
+        label_smoothing_factor=args.label_smoothing_factor,
+        remove_unused_columns=False,
+        load_best_model_at_end=True,
+        metric_for_best_model=args.metric_for_best_model,
+        seed=args.seed,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=training_dataset,
+        eval_dataset=validation_dataset,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+        callbacks=[
+            EarlyStoppingCallback(
+                early_stopping_patience=args.early_stopping_patience,
+            )
+        ],
+    )
+    
+    # train
+    trainer.train()
+    
+    # predict
+    y_pred = trainer.predict(test_dataset).predictions
+    y_preds.append(y_pred)
+    
+    # save model
+    #trainer.save_model(os.path.join(args.save_model_dir, f"fold{kfold_idx}"))
 
-        training_args = TrainingArguments(
-            output_dir=os.path.join(args.save_model_dir, f"fold{kfold_idx}"),
-            overwrite_output_dir=True,
-            evaluation_strategy="steps",
-            per_device_train_batch_size=args.batch_size,
-            per_device_eval_batch_size=args.batch_size,
-            gradient_accumulation_steps=args.gradient_accumulation_steps,
-            learning_rate=args.learning_rate,
-            num_train_epochs=args.epochs,
-            log_level="critical",
-            logging_steps=25,
-            save_strategy="steps",
-            save_steps=25,
-            save_total_limit=3,
-            label_smoothing_factor=args.label_smoothing_factor,
-            remove_unused_columns=False,
-            load_best_model_at_end=True,
-            metric_for_best_model=args.metric_for_best_model,
-            seed=args.seed + kfold_idx**2,
-            data_seed=args.seed + kfold_idx**2,
-        )
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            train_dataset=training_dataset,
-            eval_dataset=validation_dataset,
-            tokenizer=tokenizer,
-            compute_metrics=compute_metrics,
-            callbacks=[
-                EarlyStoppingCallback(
-                    early_stopping_patience=args.early_stopping_patience,
-                )
-            ],
-        )
-        trainer.train()
-
-        oof_train[val_idx] = np.argmax(trainer.predict(validation_dataset).predictions, axis=1)
-        y_pred = trainer.predict(test_dataset).predictions
-        y_preds.append(y_pred)
-
-        trainer.save_model(os.path.join(args.save_model_dir, f"fold{kfold_idx}"))
-
-    return oof_train, y_preds
+    return y_preds
 
 def main(args):
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     seed_everything(args.seed)
     
     # set data
-    # devがもともとなかった
-    x_train = load_data(DATA_PATH,"text.train.txt")
-    x_test = load_data(DATA_PATH,"text.test.txt")
-    x_dev = load_data(DATA_PATH,"text.dev.txt")
+    x_train = load_data(args.data_dir,"text.train.txt")
+    x_test = load_data(args.data_dir,"text.test.txt")
+    x_dev = load_data(args.data_dir,"text.dev.txt")
 
-    y_train = load_data(DATA_PATH,"label.train.txt")
-    y_dev = load_data(DATA_PATH,"label.dev.txt")
+    y_train = load_data(args.data_dir,"label.train.txt")
+    y_dev = load_data(args.data_dir,"label.dev.txt")
     y_train = [i+2 for i in list(map(int,y_train))]
     y_dev = [i+2 for i in list(map(int,y_dev))]
 
+    # train
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # train parameter
-    parser.add_argument("--model_name", type=str, default="cl-tohoku/bert-base-japanese-whole-word-masking")
-    parser.add_argument("--kfold", type=int, default=5)
-    parser.add_argument("--max_length", type=int, default=-1)
-    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--model_name", type=str, default="studio-ousia/luke-japanese-large")
+    parser.add_argument("--kfold", type=int, default=15)
+    parser.add_argument("--max_length", type=int, default=256)
+    parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--gradient_accumulation_steps", default=1, type=int)
     parser.add_argument("--learning_rate", type=float, default=3e-5)
-    parser.add_argument("--epochs", type=int, default=3)
+    parser.add_argument("--lr_scheduler_type", type=str, default="linear")
+    parser.add_argument("--warmup_steps", type=int, default=0)
+    parser.add_argument("--epochs", type=int, default=100)
     parser.add_argument("--label_smoothing_factor", type=float, default=0.2)
     parser.add_argument("--metric_for_best_model", type=str, default="f1_score")
-    parser.add_argument("--early_stopping_patience", type=int, default=3)
+    parser.add_argument("--steps", type=int, default=25)
+    parser.add_argument("--early_stopping_patience", type=int, default=10)
     parser.add_argument("--seed", type=int, default=42)
-    
     # PATH
-    parser.add_argument("--data_dir", default="../data")
-    parser.add_argument("--save_model_dir", default="../model")
-    parser.add_argument("--sub_file", default="../submission/sub.csv")
+    parser.add_argument("--data_dir", default="/home/kajikawa_r/competition/gradcomp/data")
+    parser.add_argument("--save_model_dir", default="/home/kajikawa_r/competition/gradcomp/ch03/model")
+    parser.add_argument("--sub_file", default="/home/kajikawa_r/competition/gradcomp/ch03/submission/sub.txt")
     args = parser.parse_args()
-
     main(args)
